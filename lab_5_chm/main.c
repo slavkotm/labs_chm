d91620e2cc59ebdc70ead44e7999b4814480bbed
KotSlava <kot.slava.mih@gmail.com>
2021-11-02 21:52:13 +0300

    commit 5 lab chm

---
+++ lab_5_chm/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void find_xs() {
+        
+}
+
+int main()
+
+
+
+    return 0;
+
+
+
+
+{    
+    
+    
+
+
+
+    
+
+
+
+
+    return 0;
+}
